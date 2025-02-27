8c32e1328bb976b63371711dbb47c98cb44534e2
Gleb Makarevich <melanholiya1@icloud.com>
2025-02-27 20:00:45 +0300

    Add Crop and OOP program delete all original info in new file

---
+++ include/imports.h
@@ -0,0 +1,24 @@
+//
+// Created by Глеб Макаревич on 27.02.2025.
+//
+
+#ifndef IMPORTS_H
+#define IMPORTS_H
+
+// Systems includes
+#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <memory>
+
+// Locale includes
+#include "parameters.h"
+#include "filter_base.h"
+#include "filters/crop_filter.h"
+#include "bmp_processor.h"
+#include "filter_factory.h"
+#endif //IMPORTS_H
