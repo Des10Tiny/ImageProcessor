82432dfddcac13897f3c3f17e590406fa459be88
Gleb Makarevich <melanholiya1@icloud.com>
2025-02-27 17:07:09 +0300

    Add class and struct of pathDate an add Reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include "path.cpp"
 
-int main(int argc, char** argv) {
+int main(const int argc, char** argv) {
     std::cout << argc << std::endl;
     for (int i = 0; i < argc; ++i) {
         std::cout << argv[i] << std::endl;
