e5a0fefa756f9606eea03ab59f09da76b35ae919
Gleb Makarevich <melanholiya1@icloud.com>
2025-03-10 21:58:48 +0300

    -gs filter now work correctly due to BGR not RGB

--- src/filters/grayscale.cpp
+++ src/filters/grayscale.cpp
@@ -19,11 +19,13 @@ void GrayscaleFilter::ProcessPartition(const std::vector<uint8_t>& image_data, s
   for (int y = start_y; y < end_y; ++y) {
     for (int x = 0; x < width; ++x) {
       const int index = (y * width + x) * 3;
-      const uint8_t r = image_data[index];
-      const uint8_t g = image_data[index + 1];
-      const uint8_t b = image_data[index + 2];
 
-      const auto gray = static_cast<uint8_t>(0.229 * r + 0.587 * g + 0.114 * b);
+      //Как оказалось формат не RGB а BGR
+      const uint8_t r = image_data[index + 2];
+      const uint8_t b = image_data[index];
+      const uint8_t g = image_data[index + 1];
+
+      const auto gray = static_cast<uint8_t>(0.299 * r + 0.587 * g + 0.114 * b);
 
       grayscale_data[index] = grayscale_data[index + 1] = grayscale_data[index + 2] = gray;
     }
